8778609860efcb319184970230bd7e591fa5f60c
Matthew Jenkins <54869556+SemanticSalt-dev@users.noreply.github.com>
2024-12-29 14:44:36 -0500

    This update reformats the nav bar at the top of the page to be horizontal and adds the word Spooky next to my name on the home page if spookiness level is found to be in Stranger range

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 let spookiness = Math.random() *  100;
 
 // if spookiness is less than 28 then assign 'Matt', other wise if less than 71 assign 'Me', otherwise assign a blank string
-let defaultName = spookiness < 28 ? 'Matt' : (spookiness < 71 ? 'Me' : '');
+let defaultName;
+
+// apply a string value to default name spookiness leaving it blank if spookier than 71%
+switch (true) {
+    case spookiness < 28:
+      defaultName = 'Matt';
+      break;
+    case spookiness < 71:
+      defaultName = 'Me';
+      break;
+    default:
+      defaultName = '';
+  }
 
 // If defaultName is empty, assign as 'Stranger', otherwise defaultName to username
+// This step remains as an example of a coalescing OR statement
 let username = defaultName || 'Stranger'; 
 
 
