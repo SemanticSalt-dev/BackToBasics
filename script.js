29d2e83f8a0576214126465ceb398704dff2659d
Matthew Jenkins <54869556+SemanticSalt-dev@users.noreply.github.com>
2024-12-29 00:30:12 -0500

    Non-working commented code was removed
    An external javascript page was added to assign a
    username and if this had been left blank it would
    be assigned to stranger. This was really just to
    play with code but a function to better decide if
    know who the user is will be added later.
    
    In the About Me page, two javascript functions
    were added to update the contents of heading one
    and two. The updates use the variable username
    from the external javascript page. These take place
    after the document finishes loading.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Code will be added to check if the user is logged in and assign the 
+let defaultName = 'Me';
+let username = defaultName || 'Stranger'; // If username is not empty, assign defaultName to 'Stranger' to stranger, otherwise username to defaultName
+
+
