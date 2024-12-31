52cc2e4e72897e29d987c73c6b69a8465a51c3ef
Matthew Jenkins <54869556+SemanticSalt-dev@users.noreply.github.com>
2024-12-31 11:11:07 -0500

    The functions written in the script tags within the index and about me pages were moved to external functions on script.js
    These functions are still called from script tags and
    using the id of the element to be updated as a
    parameter.

--- script.js
+++ script.js
@@ -20,4 +20,31 @@ switch (true) {
 // This step remains as an example of a coalescing OR statement
 let username = defaultName || 'Stranger'; 
 
+function updateSpookiness(elementId) {
+  // Calculate spookiness rounded down to the hundredths place
+  let spookyHundredthFloored = Math.floor(spookiness * 100) / 100; // Calculate spookiness rounded down to the hundredths place
+  let fullStringThree = `I'm ${spookyHundredthFloored.toFixed(2)}% spooky.`; // Create the string to display
+  const listItem = document.getElementById(elementId); // Get the list item and update its text content
+  listItem.textContent = fullStringThree;
+}
 
+function updateAboutMe(elementId) {
+  let fullStringOne = `About ${username}`; // Create the full string needed for this element
+  const headingOne = document.getElementById(elementId); //create a variable and and assign it to the document element with id="aboutMe"
+  console.log(fullStringOne); // print to console for testing - can remove
+  headingOne.textContent = fullStringOne;
+} 
+
+function updateThingsAbout(elementId) {
+  let fullStringTwo = `Some things about ${username}.`;
+  const headingTwo = document.getElementById(elementId);
+  headingTwo.textContent = fullStringTwo;
+}
+
+function booSpan(elementId) {
+  const spanOne = document.getElementById(elementId); //create a variable and and assign it to the document element with id="spook"
+    if (username === 'Stranger') {
+        let spookStringOne = ` (Spooky)`; // Create the string needed for this element
+        spanOne.textContent = spookStringOne;  // change the contents of this document element to the sting assigned to spookStringOne
+    }
+}
