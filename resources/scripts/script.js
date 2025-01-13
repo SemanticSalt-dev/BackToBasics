59aaca562aafe0603756de1d4b7b37d150bd822f
Matthew Jenkins <54869556+SemanticSalt-dev@users.noreply.github.com>
2025-01-13 00:24:12 -0500

    Added a repaint button to change the background to a random color. Javascript functions needed were added on external script page.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -50,4 +50,19 @@ function booSpan(elementId) {
     }
 }
 
+// change background color at random
+function getRandomColor(){
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function changeColor(){
+  let newColor = getRandomColor();
+  document.body.style.backgroundColor = newColor;
+}
+
+
 console.log(`Hello, Stranger!`);
